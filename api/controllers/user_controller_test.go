3c846a6485cdbb218c696249a2a5931abc105488
yadavsushil07 <susyadavb12@gmail.com>
2021-04-13 14:49:44 +0530

    last push

--- api/controllers/user_controller_test.go
+++ api/controllers/user_controller_test.go
@@ -2,28 +2,30 @@ package controllers
 
 import (
 	"fmt"
-	"io"
 	"io/ioutil"
-	"net/http"
 	"net/http/httptest"
 	"testing"
 )
 
 func TestUserProfile(t *testing.T) {
 	//
-	handler := func(w http.ResponseWriter, r *http.Request) {
-		// here we write our expected response, in this case, we return a
-		// JSON string which is typical when dealing with REST APIs
-		io.WriteString(w, "{ \"status\": \"expected service response\"}")
-	}
+	// handler := func(w http.ResponseWriter, r *http.Request) {
+	// 	// here we write our expected response, in this case, we return a
+	// 	// JSON string which is typical when dealing with REST APIs
+	// 	// io.WriteString(w, "{ \"status\": \"expected service response\"}")
+	// }
 
 	req := httptest.NewRequest("GET", "/user", nil)
 	w := httptest.NewRecorder()
-	handler(w, req)
+	UserProfile(w, req)
 
 	resp := w.Result()
 	body, _ := ioutil.ReadAll(resp.Body)
-	fmt.Println(resp.StatusCode)
-	fmt.Println(resp.Header.Get("Content-Type"))
+	// fmt.Println(resp.StatusCode)
+	// fmt.Println(resp.Header.Get("Content-Type"))
 	fmt.Println(string(body))
+
+	if resp.StatusCode != 200 {
+		t.Errorf("not working")
+	}
 }
